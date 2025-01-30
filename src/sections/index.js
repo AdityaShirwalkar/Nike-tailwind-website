34b3f468ec179898751530ddbc7df5e66aa61fb4
Aditya Shirwalkar <adityashirwalkar.26@gmail.com>
2025-01-30 16:48:02 +0530

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./sections/Footer";
+import CustomerReviews from "./sections/CustomerReviews";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    Subscribe
+}
